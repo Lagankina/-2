5c5358112049846e4e24e5c4341a4cfe0b98ba37
Lagankina <reirianet@mail.ru>
2023-02-23 17:14:56 +0300

    Алиса в деле

---
+++ Proekt_Alise/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write ("Введите имя");
+string username = Console.ReadLine();
+
+if (username =="Alise") {
+    Console.WriteLine ("Ура! это же Alise!");
+} else {
+    Console.Write ("Привет, ");
+    Console.WriteLine(username);
+}
